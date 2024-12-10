e62f2d0b66b160e54f78372091ab5f7bf13cbd1e
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2024-12-10 09:32:26 +0530

    we are use the jupitor notbook adn in this code we will analysis the ipl mathches

---
+++ 3_pandas/c0.py
@@ -0,0 +1,16 @@
+
+
+#pandas :
+'''
+    - used for manupulating and anylassis data 
+    - 
+'''
+
+#main data structure :
+''' 
+    1 Dataframes:
+        - table structure
+
+    2Series:
+        - single colum in in table
+'''
